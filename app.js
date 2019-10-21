e4065373636370dc07724a546a9d4ae9771871d2
fys <723107234@qq.com>
2019-10-21 22:35:18 +0800

    增加环境变量配置

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const fs = require('fs');
+require('dotenv').config();
+const dotenv = require('dotenv');
+const envConfig = dotenv.parse(fs.readFileSync('.env.local'));
+for (const k in envConfig) {
+  process.env[k] = envConfig[k];
+}
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
