38eb7fe9e2ffb564a6abd3bb69afc4a30c7cf116
fys <723107234@qq.com>
2019-12-08 00:18:38 +0800

    代码添加行号

--- routes/updateArticle.js
+++ routes/updateArticle.js
@@ -12,13 +12,30 @@ const md = new MarkdownIt({
   highlight: function (str, lang) {
     if (lang && hljs.getLanguage(lang)) {
       try {
-        return '<pre class="hljs"><code>' +
-          hljs.highlight(lang, str, true).value +
+        const preCode = hljs.highlight(lang, str, true).value
+        const lines = preCode.split(/\n/).slice(0, -1)
+        let html = lines.map((item, index) => {
+          return '<li><span class="line-num" data-line="' + (index + 1) + '"></span>' + item + '</li>'
+        }).join('')
+        html = '<ol>' + html + '</ol>'
+        if (lines.length > 3) {
+          html += '<b class="name">' + lang + '</b>'
+        }
+        return '<pre class="hljs"><code>' + 
+          html +
           '</code></pre>'
       } catch (__) {}
     }
 
-    return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>'
+    const preCode = md.utils.escapeHtml(str)
+    const lines = preCode.split(/\n/).slice(0, -1)
+    let html = lines.map((item, index) => {
+      return '<li><span class="line-num" data-line="' + (index + 1) + '"></span>' + item + '</li>'
+    }).join('')
+    html = '<ol>' + html + '</ol>'
+    return '<pre class="hljs"><code>' + 
+      html +
+      '</code></pre>'
   }
 })
 
