da7a889146046d2756e1571792abfd95ad40f8a7
fys <723107234@qq.com>
2019-12-18 08:28:07 +0800

    更新个人中心信息

--- routes/adminUser.js
+++ routes/adminUser.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mysql = require('./../mysql/db')
 
 router.get('/select', selectUser)
+router.post('/update', updateUser)
 
 async function selectUser(req, res, next) {
   try {
@@ -13,6 +14,23 @@ async function selectUser(req, res, next) {
       data: {
         userInfo: selectData[0]
       },
+      msg: ''
+    });
+  } catch (error) {
+    return res.json({
+      isok: false,
+      msg: error
+    });
+  }
+  next();
+}
+
+async function updateUser(req, res, next) {
+  try {
+    await mysql.query('UPDATE vue_blog_author SET authorName = ?, authorIntroduce = ?, authorEmail = ?  WHERE authorId = ?',
+      [req.body.name, req.body.introduce, req.body.email, req.body.id])
+    return res.json({
+      isok: true,
       msg: ''
     });
   } catch (error) {
