a248cc34fb9dd991cc135efa16329a742e15c5fe
fys <723107234@qq.com>
2020-01-30 14:25:01 +0800

    修复第二次添加文章失败的问题

--- routes/updateArticle.js
+++ routes/updateArticle.js
@@ -5,39 +5,6 @@ const mysql = require('./../mysql/db')
 const MarkdownIt = require('markdown-it')
 const markdownItTocAndAnchor = require('markdown-it-toc-and-anchor').default
 const hljs = require('highlight.js')
-const md = new MarkdownIt({
-  html: true,
-  linkify: true,
-  typographer: true,
-  highlight: function (str, lang) {
-    if (lang && hljs.getLanguage(lang)) {
-      try {
-        const preCode = hljs.highlight(lang, str, true).value
-        const lines = preCode.split(/\n/).slice(0, -1)
-        let html = lines.map((item, index) => {
-          return '<li><span class="line-num" data-line="' + (index + 1) + '"></span>' + item + '</li>'
-        }).join('')
-        html = '<ol>' + html + '</ol>'
-        if (lines.length) {
-          html += '<b class="name">' + lang + '</b>'
-        }
-        return '<pre class="hljs"><code>' + 
-          html +
-          '</code></pre>'
-      } catch (__) {}
-    }
-
-    const preCode = md.utils.escapeHtml(str)
-    const lines = preCode.split(/\n/).slice(0, -1)
-    let html = lines.map((item, index) => {
-      return '<li><span class="line-num" data-line="' + (index + 1) + '"></span>' + item + '</li>'
-    }).join('')
-    html = '<ol>' + html + '</ol>'
-    return '<pre class="hljs"><code>' + 
-      html +
-      '</code></pre>'
-  }
-})
 
 router.post('/', updateArticle)
 
@@ -46,6 +13,39 @@ async function updateArticle(req, res, next) {
     let subTitle = req.body.content.slice(0, 200)
     let titleArray = []
     let titleObject = {}
+    const md = new MarkdownIt({
+      html: true,
+      linkify: true,
+      typographer: true,
+      highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+          try {
+            const preCode = hljs.highlight(lang, str, true).value
+            const lines = preCode.split(/\n/).slice(0, -1)
+            let html = lines.map((item, index) => {
+              return '<li><span class="line-num" data-line="' + (index + 1) + '"></span>' + item + '</li>'
+            }).join('')
+            html = '<ol>' + html + '</ol>'
+            if (lines.length) {
+              html += '<b class="name">' + lang + '</b>'
+            }
+            return '<pre class="hljs"><code>' +
+              html +
+              '</code></pre>'
+          } catch (__) {}
+        }
+
+        const preCode = md.utils.escapeHtml(str)
+        const lines = preCode.split(/\n/).slice(0, -1)
+        let html = lines.map((item, index) => {
+          return '<li><span class="line-num" data-line="' + (index + 1) + '"></span>' + item + '</li>'
+        }).join('')
+        html = '<ol>' + html + '</ol>'
+        return '<pre class="hljs"><code>' +
+          html +
+          '</code></pre>'
+      }
+    })
     let articleContentHtml = md.use(markdownItTocAndAnchor, {
       tocCallback: function (tocMarkdown, tocArray, tocHtml) {
         titleArray = tocArray
