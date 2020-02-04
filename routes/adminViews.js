c7384d2152b9d8b344faa4c180e36f4081b9ee26
fys <723107234@qq.com>
2020-02-04 14:14:17 +0800

    访问量信息完善

--- routes/adminViews.js
+++ routes/adminViews.js
@@ -5,6 +5,7 @@ const moment = require('moment')
 
 router.post('/add', addViews)
 router.get('/select', selectViews)
+router.get('/selectDetail', selectViewsDetail)
 
 async function addViews(req, res, next) {
   try {
@@ -25,12 +26,35 @@ async function addViews(req, res, next) {
 
 async function selectViews(req, res, next) {
   try {
-    let selectData = await mysql.query("SELECT DATE_FORMAT(time, '%Y-%m-%d') as time, COUNT(*) as views FROM vue_blog_views WHERE DAYOFYEAR(time) >= DAYOFYEAR(?) AND DAYOFYEAR(time) <= DAYOFYEAR(?) GROUP BY DATE_FORMAT(time, '%Y-%m-%d')",
+    let selectData = await mysql.query("SELECT DATE_FORMAT(time, '%Y-%m-%d') as time, COUNT(*) as views FROM vue_blog_views WHERE DATEDIFF(time, ?) >= 0 AND DATEDIFF(?, time) >= 0 GROUP BY DATE_FORMAT(time, '%Y-%m-%d')",
       [req.query.start, req.query.end])
-    console.log(selectData)
     return res.json({
       isok: true,
       data: selectData,
+      msg: ''
+    });
+  } catch (error) {
+    return res.json({
+      isok: false,
+      msg: error
+    });
+  }
+  next();
+}
+
+async function selectViewsDetail(req, res, next) {
+  try {
+    let limitNumber = req.query.limit * 1 || 10
+    let offsetNumber = (req.query.page * 1 - 1) * limitNumber
+    let selectData = await mysql.query("SELECT routeFrom, routeTo, DATE_FORMAT(time, '%Y-%m-%d %r') as time FROM vue_blog_views WHERE DATEDIFF(time, ?) >= 0 AND DATEDIFF(?, time) >= 0 ORDER BY time DESC LIMIT ? OFFSET ?",
+      [req.query.start, req.query.end, limitNumber, offsetNumber])
+    let totalData = await mysql.query("SELECT * FROM vue_blog_views WHERE DATEDIFF(time, ?) >= 0 AND DATEDIFF(?, time) >= 0", [req.query.start, req.query.end])
+    return res.json({
+      isok: true,
+      data: {
+        selectData,
+        total: totalData.length
+      },
       msg: ''
     });
   } catch (error) {
