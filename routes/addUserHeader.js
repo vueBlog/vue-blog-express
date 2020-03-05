51c4623d147c168642487fec8009bea1d7d5bd88
fys <723107234@qq.com>
2020-03-05 22:28:50 +0800

    上传图片采用七牛云

--- routes/addUserHeader.js
+++ routes/addUserHeader.js
@@ -1,36 +1,38 @@
 const express = require('express')
 const router = express.Router()
 const mysql = require('../mysql/db')
-const multer = require('multer')
+const qiniu = require('qiniu')
+const {
+  config
+} = require('../config.js')
 
-let path = 'public/images/header'
-let currentPath
-let storage = multer.diskStorage({
-  // 如果你提供的 destination 是一个函数，你需要负责创建文件夹
-  destination: path,
-  // 给上传文件重命名，获取添加后缀名
-  filename: function (req, file, cb) {
-    currentPath = `id-${req.body.id}.${file.mimetype.slice(6)}`
-    cb(null, currentPath)
+router.get('/getToken', getToken)
+router.post('/add', addImage)
+
+async function getToken(req, res, next) {
+  const mac = new qiniu.auth.digest.Mac(config.accessKey, config.secretKey);
+  const options = {
+    scope: `${config.bucket}`,
+    expires: 7200
   }
-})
-let upload = multer({
-  storage: storage
-})
-
-router.post('/', upload.single('file'), addImage)
+  const putPolicy = new qiniu.rs.PutPolicy(options);
+  const uploadToken = putPolicy.uploadToken(mac);
+  res.json({
+    isok: true,
+    msg: '上传凭证获取成功',
+    upToken: uploadToken
+  });
+}
 
 async function addImage(req, res, next) {
   try {
-    let imgPath = `images/header/${currentPath}`
+    let imgPath = req.body.headerPath
     await mysql.query('UPDATE vue_blog_author SET authorHeadimg = ? WHERE authorId = ?',
       [imgPath, req.body.id]);
     return res.json({
       isok: true,
       msg: '',
-      data: {
-        src: imgPath
-      }
+      data: {}
     });
   } catch (error) {
     return res.json({
--- routes/images.js
+++ routes/images.js
@@ -1,79 +1,26 @@
 const express = require('express')
 const router = express.Router()
-const mysql = require('../mysql/db')
-const multer = require('multer')
+const qiniu = require('qiniu')
+const {
+  config
+} = require('../config.js')
 
-//获取时间
-function getNowFormatDate() {
-  const date = new Date()
-  const seperator1 = "-"
-  let month = date.getMonth() + 1
-  let strDate = date.getDate()
-  if (month >= 1 && month <= 9) {
-    month = "0" + month
-  }
-  if (strDate >= 0 && strDate <= 9) {
-    strDate = "0" + strDate
-  }
-  let currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
-  return currentdate.toString()
-}
-let datatime = getNowFormatDate()
-let path = 'public/images/' + datatime
-let storage = multer.diskStorage({
-  // 如果你提供的 destination 是一个函数，你需要负责创建文件夹
-  destination: path,
-  // 给上传文件重命名，获取添加后缀名
-  filename: function (req, file, cb) {
-    cb(null, `${Date.now()}-${Math.floor(Math.random() * 100000000)}.${file.mimetype.slice(6)}`)
-  }
-})
-let upload = multer({
-  storage: storage
-})
-
-router.post('/add', upload.array('image', 5), addImage)
-// router.post('/delete', deleteImage)
+router.get('/add', addImage)
 
 async function addImage(req, res, next) {
-  try {
-    const files = req.files
-    if (!files[0]) {
-      return res.json({
-        isok: false,
-        msg: err
-      });
-    } else {
-      let imgPath = `images/${datatime}/${files[0].filename}`
-      await mysql.query('INSERT INTO vue_blog_img (articleId, imgPath) VALUES (?, ?)',
-        [req.body.articleId, imgPath])
-      return res.json({
-        isok: true,
-        msg: '',
-        data: {
-          src: imgPath
-        }
-      });
-    }
-  } catch (error) {
-    return res.json({
-      isok: false,
-      msg: error
-    });
+  const mac = new qiniu.auth.digest.Mac(config.accessKey, config.secretKey);
+  const options = {
+    scope: config.bucket,
+    expires: 7200
   }
+  const putPolicy = new qiniu.rs.PutPolicy(options);
+  const uploadToken = putPolicy.uploadToken(mac);
+  res.json({
+    isok: true,
+    msg: '上传凭证获取成功',
+    upToken: uploadToken
+  });
   next();
 }
-
-// async function deleteImage(req, res, next) {
-//   try {
-
-//   } catch (error) {
-//     return res.json({
-//       isok: false,
-//       msg: error
-//     });
-//   }
-//   next();
-// }
 
 module.exports = router;
