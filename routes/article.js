2b4433b78ec7da46b3fb1a862e5c274582d9fb4a
fys <723107234@qq.com>
2020-03-12 22:03:02 +0800

    代码块优化

--- routes/article.js
+++ routes/article.js
@@ -21,32 +21,30 @@ async function addArticle(req, res, next) {
       linkify: true,
       typographer: true,
       highlight: function (str, lang) {
+        const codeIndex = parseInt(Date.now()) + Math.floor(Math.random() * 10000000)
+        let html = `<button class="copy-btn" type="button" data-clipboard-action="copy" data-clipboard-target="#copy${codeIndex}">复制</button>`
+        const linesLength = str.split(/\n/).length - 1
+        let linesNum = '<span aria-hidden="true" class="line-numbers-rows">'
+        for (let index = 0; index < linesLength; index++) {
+          linesNum = linesNum + '<span></span>'
+        }
+        linesNum += '</span>'
         if (lang && hljs.getLanguage(lang)) {
           try {
             const preCode = hljs.highlight(lang, str, true).value
-            const lines = preCode.split(/\n/).slice(0, -1)
-            let html = lines.map((item, index) => {
-              return '<li><span class="line-num" data-line="' + (index + 1) + '"></span>' + item + '</li>'
-            }).join('')
-            html = '<ol>' + html + '</ol>'
-            if (lines.length) {
+            html = html + preCode
+            if (linesLength) {
               html += '<b class="name">' + lang + '</b>'
             }
-            return '<pre class="hljs"><code>' +
-              html +
-              '</code></pre>'
-          } catch (__) {}
+            return `<pre class="hljs"><code>${html}</code>${linesNum}</pre><textarea style="position: absolute;top: -9999px;left: -9999px;z-index: -9999;" id="copy${codeIndex}">${str}</textarea>`
+          } catch (error) {
+            console.log(error)
+          }
         }
 
         const preCode = md.utils.escapeHtml(str)
-        const lines = preCode.split(/\n/).slice(0, -1)
-        let html = lines.map((item, index) => {
-          return '<li><span class="line-num" data-line="' + (index + 1) + '"></span>' + item + '</li>'
-        }).join('')
-        html = '<ol>' + html + '</ol>'
-        return '<pre class="hljs"><code>' +
-          html +
-          '</code></pre>'
+        html = html + preCode
+        return `<pre class="hljs"><code>${html}</code>${linesNum}</pre><textarea style="position: absolute;top: -9999px;left: -9999px;z-index: -9999;" id="copy${codeIndex}">${str}</textarea>`
       }
     })
     let articleContentHtml = md.use(markdownItTocAndAnchor, {
