e25a3c156c496306aa489af8953ff2a48e90a514
fys <723107234@qq.com>
2020-03-14 21:40:29 +0800

    修复 textarea 的渲染问题

--- routes/article.js
+++ routes/article.js
@@ -36,7 +36,7 @@ async function addArticle(req, res, next) {
             if (linesLength) {
               html += '<b class="name">' + lang + '</b>'
             }
-            return `<pre class="hljs"><code>${html}</code>${linesNum}</pre><textarea style="position: absolute;top: -9999px;left: -9999px;z-index: -9999;" id="copy${codeIndex}">${str}</textarea>`
+            return `<pre class="hljs"><code>${html}</code>${linesNum}</pre><textarea style="position: absolute;top: -9999px;left: -9999px;z-index: -9999;" id="copy${codeIndex}">${str.replace(/<\/textarea>/g, '&lt;textarea>')}</textarea>`
           } catch (error) {
             console.log(error)
           }
@@ -44,7 +44,7 @@ async function addArticle(req, res, next) {
 
         const preCode = md.utils.escapeHtml(str)
         html = html + preCode
-        return `<pre class="hljs"><code>${html}</code>${linesNum}</pre><textarea style="position: absolute;top: -9999px;left: -9999px;z-index: -9999;" id="copy${codeIndex}">${str}</textarea>`
+        return `<pre class="hljs"><code>${html}</code>${linesNum}</pre><textarea style="position: absolute;top: -9999px;left: -9999px;z-index: -9999;" id="copy${codeIndex}">${str.replace(/<\/textarea>/g, '&lt;textarea>')}</textarea>`
       }
     })
     let articleContentHtml = md.use(markdownItTocAndAnchor, {
